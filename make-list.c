ba3aaaa10c93198785bd83caef30a198f80c2135
Colin Targonski <colin@182.242.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-10 18:38:54 -0400

    first commit

---
+++ make-list.c
@@ -0,0 +1,66 @@
+/*
+ * This program generates a vector binary file that first lists the number of 
+ * elements in the vector followed by the values
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	int opt, num = -1, *r_nums, i;
+	char *out_file = NULL;
+	FILE *fp = NULL;
+
+	// seed the random number generator
+	srand(time(NULL));
+
+	while ((opt = getopt(argc, argv, "n:o:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'n':
+				num = atoi(optarg);
+				if (num <= 0)
+				{
+					printf("Invalid number of elements, exiting\n");
+					exit(1);
+				}
+				break;
+			case 'o':
+				out_file = (char *) malloc (sizeof(char) * 100);
+				strcpy(out_file, optarg);
+				break;
+			case '?':
+				printf("Incorrect argument(s), exiting\n");
+				exit(1);
+		}
+	}
+
+	if (num == -1) num = 100; // default number if argument is not provided
+	if (out_file == NULL)
+	{
+		out_file = (char *) malloc (sizeof(char) * 100);
+		strcpy(out_file, "default-list-file.dat");
+	} 
+
+	r_nums = (int *) malloc (sizeof(int) * num);
+	
+	for (i = 0; i < num; i++)
+	{
+		r_nums[i] = rand() % 10000;
+	}
+
+	fp = fopen(out_file, "wb");
+	fwrite(&num, sizeof(int), 1, fp);
+	fwrite(r_nums, sizeof(int), num, fp);
+	fclose(fp);
+
+	free(r_nums);
+	free(out_file);
+
+	return 0;
+}
---
+++ print-list.c
@@ -0,0 +1,59 @@
+/*
+ * This program prints a vector binary file that first lists the number of 
+ * elements in the vector followed by the values
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i, opt, num, *vals;
+	char *print_file = NULL;
+	FILE *fp = NULL;
+
+
+	while ((opt = getopt(argc, argv, "i:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'i':
+				print_file = (char *) malloc (sizeof(char) * 100);
+				strcpy(print_file, optarg);
+				break;
+			case '?':
+				printf("Incorrect argument(s), exiting\n");
+				exit(1);
+		}
+	}
+
+	if (print_file == NULL)
+	{
+		print_file = (char *) malloc (sizeof(char) * 100);
+		strcpy(print_file, "default-list-file.dat");
+	}
+
+	if ((fp = fopen(print_file, "rb")) == NULL)
+	{
+		printf("Cannot open file %s for printing, exiting\n", print_file);
+		exit(1);
+	}
+
+	fread(&num, sizeof(int), 1, fp);
+	printf("%d\n", num);
+
+	vals = (int *) malloc (sizeof(int) * num);
+	fread(vals, sizeof(int), num, fp);
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%d\n", vals[i]);
+	}
+
+	free(vals);
+	free(print_file);
+
+	return 0;
+}
